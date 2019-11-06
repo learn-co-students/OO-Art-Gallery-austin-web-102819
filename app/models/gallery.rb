f6d9d40c63e493bc2f238743188638883e424277
Buford Eeds <bufordeeds8@gmail.com>
2019-11-06 14:27:36 -0600

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,34 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select{|p| p.gallery == self}
+  end
+
+  def artists
+    my_p = self.paintings
+    my_p.map {|p|p.artist}.uniq
+  end
+
+  def artist_names
+    self.artists.map {|a|a.name}.uniq
+  end
+
+  def most_expensive_painting
+    paintings.max {|p| p.price}
+
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery, :artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery = gallery
+    @artist = artist
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    all.map{|p| p.price}.reduce(0) {|sum, num| sum + num}
   end
 
 end
