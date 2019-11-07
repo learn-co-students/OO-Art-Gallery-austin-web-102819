ee56f0a05093541bdd628b817a1f51e0451c4af1
crayray <rach.fuerst@gmail.com>
2019-11-06 19:31:39 -0600

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
 
   attr_reader :title, :price
-
+  attr_accessor :gallery, :artist
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @gallery= gallery
+    @artist= artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    price= self.all.map {|painting| painting.price}
+    price.reduce {|sum, num| sum + num}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,20 @@
 require_relative '../config/environment.rb'
 
 
+# artist
+john= Artist.new("John", 10)
+judy= Artist.new("Judy", 20)
+lucy= Artist.new("Lucy", 10)
+
+#gallery
+canopy= Gallery.new("Canopy", "ATX")
+
+johnspaintint= john.create_painting("Hellooooo", 100000, canopy)
+johnspainting2= john.create_painting("NOOOOOO", 10000, canopy)
+judyspainting= judy.create_painting("YESSS", 1000000, canopy)
+judyspainting2= judy.create_painting("BYE BITCH", 12, canopy)
+lucyspainting= lucy.create_painting("GETOUTTAHERE", 25, canopy)
+lucyspainting2= lucy.create_painting("I HATE THIS", 19, canopy)
 
 
 binding.pry
